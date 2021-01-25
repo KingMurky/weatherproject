eb6e1a17dade4870adaf045aff51673bbb44376b
Murking <staryunleegh@gmail.com>
2021-01-25 22:47:26 +0900

    에러 페이지 만들고 기본 틀만 잡아둠. 그리고 날씨 페이지 디자인 수정

--- weather/urls.py
+++ weather/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('current/',views.current, name='current'),
-    path('display/',views.display, name='display')
+    path('display/',views.display, name='display'),
 ]
